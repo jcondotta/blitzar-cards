31c12b0b3c47386eae43654f509b4a0bad645034
Jefferson Condotta <j.condotta@complianceandrisks.com>
2023-03-07 16:51:06 +0100

    initial addCard service + tests

---
+++ src/main/java/com/blitzar/cards/service/AddCardDelegate.java
@@ -0,0 +1,13 @@
+package com.blitzar.cards.service;
+
+import java.time.LocalDate;
+
+public interface AddCardDelegate {
+
+    String getCardholderName();
+
+//    enum CardStatus {
+//        ACTIVE, CANCELLED, BLOCKED
+//    }
+
+}
