d77d3f060a7cef53c95ecaaae26438b55a62bad5
Jefferson Condotta <j.condotta@complianceandrisks.com>
2023-03-07 16:48:18 +0100

    Card entity + its repository

---
+++ src/main/java/com/blitzar/cards/domain/Card.java
@@ -0,0 +1,63 @@
+package com.blitzar.cards.domain;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+public class Card {
+
+    @Id
+    @Column(name = "card_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long cardId;
+
+    @Column(name = "cardholder_name")
+    private String cardholderName;
+
+    @Column(name = "card_number")
+    private String cardNumber;
+
+    @Column(name = "expiration_date")
+    private LocalDate expirationDate;
+
+    @Column(name = "security_code")
+    private String securityCode;
+
+    public Long getCardId() {
+        return cardId;
+    }
+
+    public String getCardholderName() {
+        return cardholderName;
+    }
+
+    public void setCardholderName(String cardholderName) {
+        this.cardholderName = cardholderName;
+    }
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(String cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public LocalDate getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(LocalDate expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+
+    public String getSecurityCode() {
+        return securityCode;
+    }
+
+    public void setSecurityCode(String securityCode) {
+        this.securityCode = securityCode;
+    }
+}
