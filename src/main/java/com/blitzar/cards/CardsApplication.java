2eed5167eaeaf52ef68a8d545c5a29e95f394a1a
Jefferson Condotta <j.condotta@complianceandrisks.com>
2023-03-07 16:48:49 +0100

    i18n - messages and exceptions

--- src/main/java/com/blitzar/cards/CardsApplication.java
+++ src/main/java/com/blitzar/cards/CardsApplication.java
@@ -2,6 +2,13 @@ package com.blitzar.cards;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class CardsApplication {
@@ -10,4 +17,28 @@ public class CardsApplication {
 		SpringApplication.run(CardsApplication.class, args);
 	}
 
+	@Bean
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
+		sessionLocaleResolver.setDefaultLocale(Locale.UK);
+		return sessionLocaleResolver;
+	}
+
+	@Bean
+	public MessageSource messageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasenames("i18n/messages/messages");
+		messageSource.setDefaultEncoding("UTF-8");
+		messageSource.setDefaultLocale(Locale.UK);
+		return messageSource;
+	}
+
+	@Bean("exceptionMessageSource")
+	public MessageSource exceptionMessageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasenames("i18n/exceptions/exceptions");
+		messageSource.setDefaultEncoding("UTF-8");
+		messageSource.setDefaultLocale(Locale.UK);
+		return messageSource;
+	}
 }
